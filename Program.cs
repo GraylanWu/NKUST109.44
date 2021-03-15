16b59eabd8126b5d967f8d2edd56915f4264ae71
GraylanWu <80704420+GraylanWu@users.noreply.github.com>
2021-03-16 00:22:03 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MediaSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
